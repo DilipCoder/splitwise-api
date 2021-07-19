d6298e8101eaa2e32fcb112d4a7843f16869f875
DilipCoder <krdilip276@gmail.com>
2021-07-19 21:24:55 +0530

    remote database added

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,10 +1,17 @@
 import config from 'config';
 import { dbConfig } from '@interfaces/db.interface';
 
-const { host, port, database }: dbConfig = config.get('dbConfig');
+const env: string = config.get('env');
+const { host, port, database, password }: dbConfig = config.get('dbConfig');
+
+// db setup as local and hosted as per node environment (.env)
+const url =
+  env === 'test'
+    ? `mongodb+srv://tempUser:${password}@cluster0.groyx.mongodb.net/${database}?retryWrites=true&w=majority`
+    : `mongodb://${host}:${port}/${database}`;
 
 export const dbConnection = {
-  url: `mongodb://${host}:${port}/${database}`,
+  url,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
